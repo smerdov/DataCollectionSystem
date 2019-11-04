d0f833b654cae7d67c4102c184348b7fcfdbbfec
Anton Smerdov <neonx11ant@gmail.com>
2019-11-04 15:40:13 +0100

    upd

--- Analysis/config.py
+++ Analysis/config.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 dataset_folder = '../Dataset/'
 data_folder = '../Data/'
 pic_folder = '../Pictures/'
+API_KEY = 'RGAPI-65a54bee-f88c-4f9f-b424-ba90f31f47a5'
 
 plt.interactive(True)
 pd.options.display.max_columns = 15
