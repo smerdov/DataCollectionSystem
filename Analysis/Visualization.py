b2b9c2d194539586cd265488ae568c68d8c70281
Anton Smerdov <neonx11ant@gmail.com>
2019-11-06 10:25:54 +0100

    visualization

--- Analysis/Visualization.py
+++ Analysis/Visualization.py
@@ -5,13 +5,19 @@ import os
 import matplotlib.pyplot as plt
 import itertools
 from config import *
+from utils import get_intervals_from_moments, EventIntervals
 
 
-data_dict = joblib.load(data_folder + 'data_dict')
-
+plt.interactive(False)
+# data_dict = joblib.load(data_folder + 'data_dict')
+matches_dict = joblib.load(data_folder + 'matches_dict')
+# replays_dict = joblib.load(data_folder + 'replays_dict')
 
 # del data_dict[0]['eye_tracker']
 # del data_dict[1]['eye_tracker']
+
+interval_start = -1
+interval_end = 1
 
 
 def square_plot(df, columns2plot, timecol='Timestamp', suffix='last'):
@@ -35,9 +41,9 @@ def square_plot(df, columns2plot, timecol='Timestamp', suffix='last'):
 data_sources = [f'arduino_{i}' for i in range(3)] + ['eye_tracker']
 
 data_sources_columns2plot = {
-    'arduino_0': list(data_dict[0]['arduino_0'].columns),
-    'arduino_1': list(data_dict[0]['arduino_1'].columns),
-    'arduino_2': list(data_dict[0]['arduino_2'].columns),
+    'arduino_0': list(matches_dict['4232819529']['0']['sensors']['arduino_0'].columns),
+    'arduino_1': list(matches_dict['4232819529']['0']['sensors']['arduino_1'].columns),
+    'arduino_2': list(matches_dict['4232819529']['0']['sensors']['arduino_2'].columns),
     'eye_tracker': ['gaze_x', 'gaze_y', 'pupil_diameter'],
 }
 
@@ -46,35 +52,83 @@ n_plots = sum([len(value) for value in data_sources_columns2plot.values()])
 n_cols = np.ceil(np.sqrt(n_plots)).astype(int)
 n_rows = (n_plots - 1) // n_cols + 1
 
-for player_id, data_dict4player in data_dict.items():
-    fig, ax = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(50, 25), squeeze=False, sharex=True)
-    n_plot = 0
 
-    for data_source in data_sources:
-        df2plot = data_dict4player[data_source]
-        # # df = data_dict4player[data_source]
-        # df2plot.index = pd.to_datetime(df2plot.index)
-        # df2plot = df2plot.resample('1s').mean()
+for match_id, match_dict in matches_dict.items():
+    # for player_id, match_dict4player in match_dict.items():
+    for player_id in player_ids:
+        match_dict4player = match_dict[player_id]
 
-        if data_source in data_sources_columns2plot:
-            columns2plot = data_sources_columns2plot[data_source]
-        else:
-            columns2plot = df2plot.columns
+        fig, ax = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(50, 25), squeeze=False, sharex=True)
+        n_plot = 0
 
-        for column in columns2plot:
-            print(f'column {column} done')
-            data2plot = df2plot.loc[:, column]
+        for data_source in data_sources:
+            df2plot = match_dict4player['sensors'][data_source]
+            # # df = data_dict4player[data_source]
+            # df2plot.index = pd.to_datetime(df2plot.index)
+            # df2plot = df2plot.resample('1s').mean()
 
-            n_row = n_plot // n_cols
-            n_col = n_plot % n_cols
-            ax[n_row, n_col].plot(data2plot)
+            if data_source in data_sources_columns2plot:
+                columns2plot = data_sources_columns2plot[data_source]
+            else:
+                columns2plot = df2plot.columns
 
-            ax[n_row, n_col].set_title(column)
+            for column in columns2plot:
+                print(f'column {column} done')
+                data2plot = df2plot.loc[:, column]
+                data2plot.index = data2plot.index.total_seconds()
 
-            n_plot += 1
+                n_row = n_plot // n_cols
+                n_col = n_plot % n_cols
+                ax[n_row, n_col].plot(data2plot, color='black', alpha=0.5)
+                title = column
+                if data_source == 'arduino_1':  # chair
+                    title = 'chair ' + title
+                ax[n_row, n_col].set_title(title)
+                if n_row == n_rows - 1:
+                    ax[n_row, n_col].set_xlabel('Time, s')
 
-    fig.tight_layout()
-    fig.savefig(f'{pic_folder}player_{player_id}')
+                n_plot += 1
+
+                # intervals_dict = {}
+                event_intervals_list = []
+                events_colors_list = [('kill', 'green'), ('death', 'red'), ('assist', 'blue')]
+                # for event_type in ['kill', 'death', 'assist']:
+                for event_type, color in events_colors_list:
+                    # intervals_dict[event_type] = get_intervals_from_moments(match_dict4player['events'][f'{event_type}_times'],
+                    #                                                         interval_start=interval_start, interval_end=interval_end)
+                    intervals_list = get_intervals_from_moments(match_dict4player['events'][f'{event_type}_times'],
+                                                                            interval_start=interval_start, interval_end=interval_end)
+
+                    event_intervals4event_type = EventIntervals(intervals_list=intervals_list, label=event_type, color=color)
+                    event_intervals_list.append(event_intervals4event_type)
+
+                for event_intervals in event_intervals_list:
+
+                    # intervals_list = event_intervals.intervals_list
+                    event_label = event_intervals.label
+                    color = event_intervals.color
+                    # mask_interval_list = get_mask_intervals(times, intervals_list=intervals_list)
+                    mask_interval_list = event_intervals.get_mask_intervals(data2plot.index)
+
+                    for mask_interval in mask_interval_list:
+                        # times_with_mask = data2plot.index[mask_interval]
+                        data2plot_with_mask = data2plot.loc[mask_interval]
+                        ax[n_row, n_col].plot(
+                            # times_with_mask,
+                            data2plot_with_mask,
+                            # label=event_label,
+                            color=color,
+                            alpha=0.8,
+                            # linewidth=2,
+                        )
+
+                    ax[n_row, n_col].plot([], [], label=event_label, color=color)
+
+                ax[n_row, n_col].legend(loc='upper right')
+
+        fig.suptitle(f'Match {match_id}, Player {player_id}')
+        fig.tight_layout(rect=[0, 0, 1, 0.97])
+        fig.savefig(f'{pic_folder}match_{match_id}_player_{player_id}.png')
 
 
 
@@ -82,9 +136,7 @@ for player_id, data_dict4player in data_dict.items():
 
 
 
-
-
-square_plot(df, columns2plot=df.columns[1:], suffix=arduino_name)
+# square_plot(df, columns2plot=df.columns[1:], suffix=arduino_name)
 
 
 
