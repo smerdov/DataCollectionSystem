15644de97f142b690abc6332dafb412ddfb59cad
Anton Smerdov <neonx11ant@gmail.com>
2019-11-07 09:17:38 +0100

    thermal data visualization

---
+++ Analysis/ThermalDataVisualization.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+from config import *
+
+# plt.interactive(False)
+
+thermal_images_path = '../AndroidAppData/'
+filenames = os.listdir(thermal_images_path)
+
+for filenum, filename in enumerate(filenames):
+    try:
+        thermal_data = pd.read_csv(thermal_images_path + filename, header=None, sep=';').values
+        # thermal_data.astype(float)
+        thermal_data = thermal_data.T
+
+        # plt.close()
+        plt.close()
+        fig, ax = plt.subplots()
+        ax.imshow(thermal_data, vmax=40, vmin=30, cmap='Blues')
+        fig.show()
+        fig.savefig(f'{pic_folder}thermal_image_{filenum}.png')
+    except:
+        print(f'Can\'t read {filename}')
+
+
+
